31214645a8449a2e97f3be3e3010734960e46e5f
Hunter Damron <hdamron1594@yahoo.com>
2017-01-03 22:10:16 -0500

    Updated Intro day 2 with basic syntax, math coming soon

--- Intro/Code-Examples/Jan5/01-syntax_example.py
+++ Intro/Code-Examples/Jan5/01-syntax_example.py
@@ -7,7 +7,7 @@ import math   # This is an import.
 # Imports say to use a library of someone else's code.
 # That way you don't have to reinvent the wheel. 
 
-##### Variables and assignment #####
+##### Variables and Assignment #####
 
 dogs = 8
 # This statement assigns the value 8 to the variable beans
@@ -16,6 +16,8 @@ dogs = 8
 # '=' means that the value on the right side is stored into the variable on the left side
 #       Unlike in math, the left side must always be a variable
 # '8' is a number value to be stored in the variable 'dogs'
+#       This is called a literal because it literally means the number 8
+#       Literals are any data which are not variable (i.e. number 9 or string "hello")
 
 cats = 32
 # Like the above statement, this statement stores the value 32 in a variable called cats
@@ -36,14 +38,14 @@ print(dogs) # displays "19"
 #       it treats dogs as a variable instead of literally printing "dogs"
 # The variable dogs contains 19 so the number 19 is displayed on the screen
 
-##### Tabbed Lived Matter #####
+##### Importance of Tabs #####
 
     # It is important to note that unlike in other languages, spaces matter
     # If a statement is tabbed over like this, it will cause an error
     # You will learn the appropriate times for tabbing code later in this course
     #       (For the curious, it is to signify blocks of code inside loops and conditionals)
 
-##### Data Types #####
+##### Basic Data Types #####
 
 int_example = 5
 # This variable stores 5, an integer (appreviated "int"), meaning it is a whole number
@@ -56,7 +58,16 @@ float_example = 4.9
 #       (This was not the case in Python 2 where division between ints produced an integer answer; i.e. 4 / 3 => 1)
 
 string_example = "five"
+other_string_example = 'six'
 # Strings are collections of characters and are not numeric.
 # Strings behave similarly to lists and other collections you will learn later
+# String literals can be written with double or single quotation marks
 
+boolean_example = True
+opposite_boolean_example = False
+# Booleans are not necessary now, but they will be useful for if...then... later in the course
+# They have two values 'True' and 'False' (these must be capitalized) which are, hopefully, self-explanatory
+
+# Python includes other builtin data types, but they will be discussed later
+#       For the curious, they are tuples, lists, maps, sets, and complex numbers
 
