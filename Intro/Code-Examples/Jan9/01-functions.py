89c2267a4696f1dd0c4b6950f3eeae10d2015561
Hunter Damron <hdamron1594@yahoo.com>
2017-01-09 17:42:49 -0500

    Added intro lesson jan 9

--- Intro/Code-Examples/Jan9/01-functions.py
+++ Intro/Code-Examples/Jan9/01-functions.py
@@ -1,2 +1,69 @@
 ### 01-functions.py
 # This code example shows the declaration and use of functions
+
+def knockoff_hypot(x, y):
+    return sqrt(x**2 + y**2)
+# This is a simple function (it does the exact same as math.hypot() )
+# To declare a function, you use the 'def' keyword in the format above
+# The colon indicates that the following block of code will be the contents of the function
+#       The code block is denoted by the tab
+# The return value of the function is denoted by the 'return' keyword followed by the value to be returned
+
+def show_square(n):
+    squared_value = n**2
+    print(squared_value)
+# This function does not have a return value, like the print() function
+# Instead, it does something to change the state of the computer (in this case by printing)
+#       This type of function is not common in a functional language but is possible
+
+def weird_function(a, b, c):
+    """
+    Calculates the sum of squares divided by three
+    :param a: The first parameter
+    :param b: The second parameter
+    :param c: The third parameter
+    :return: Returns the sum of a squared, b squared, and c squared
+    """
+    ret = a**2 + b**2 + c**2
+    return ret
+# This function is like others in theory, but it shows the proper documention
+# Documenation is represented by a multiline comment (in this setting it is a "docstring") immediately after function declaration in format:
+"""
+Desciption of function goal
+:param variable: Parameters descriptions
+...
+:return: Specific return value description
+"""
+# parameters are denoted by ":param" and return value is denoted by ":return" as shown above
+
+import math
+def sine_plus_cosine(theta):
+    """
+    Calculates the sum of sin(theta) and cos(theta)
+    :param theta: Angle measure (radians)
+    :return: Returns sum of sine and cosine
+    """
+    return math.sin(theta) + math.cos(theta)
+# Functions can use imported modules too
+# They can be imported inside the function, but it's better to not
+
+def multireturn_example(a, b):
+    """
+    Returns a and b concatenated and the lengths of both strings
+    :param a: First string to concatenate
+    :param b: Second string to concatenate
+    :return: Returns a concatenated with b
+    """
+    a_str = str(a)
+    b_str = str(b)
+    return (a_str + b_str, len(a_str), len(b_str))
+# Returns multiple values as a tuple (we'll discuss it very shortly)
+#       Denoted by putting multiple values in parentheses and comma separated
+
+multireturn_tuple = multireturn_example("Guten " + "Tag")
+# multireturn_tuple contains ("Guten Tag", 6, 3)
+# access individual values in tuple by "indexing" with square brackets
+#       Indexes start with 0 (you'll see soon)
+print(multireturn_tuple[0]) # prints "Guten Tag"
+print(multireturn_tuple[1]) # prints "6"
+print(multireturn_tuple[2]) # prints "3"
--- Intro/Code-Examples/Jan9/02-collections.py
+++ Intro/Code-Examples/Jan9/02-collections.py
@@ -1,2 +1,56 @@
 ### 02-collections.py
 # This example script shows the basic use of collections
+
+## Types of collections
+tuple_example = (2, 9, 49, 2.3, "number")
+list_example = [3, 3, 3.3, "3"]
+string_example = "hello"
+dictionary_example = {"five": 5, "six": 6, "nein", "no"}
+
+print(tuple_example[2]) # prints "49"
+# indexes start at 0 so index [2] is the third item
+
+list_example[3] = "three"
+# sets the value of index [3] a new value
+# strings and tuples are "immutable" meaning it cannot be changed
+#       Indexes cannot be set to new values and the length cannot change
+
+print(len(tuple_example)) # prints "5"
+# len() prints the length of the collection
+# works on any collection
+
+print(tuple_example[0:3]) # prints "(2, 9, 49)"
+# separating two indexes by a colon does "slicing"
+# returns the a collection (same type as original) containing the indexes from the first index to (but not including) the second index
+
+print(tuple_example[0:4:2]) # prints "(2, 49)"
+# a third index in the slice makes it go by that number (in this case, every two)
+
+print(dictionary_example["five"]) # prints "5"
+# with dictionaries, keys are used instead of indexes
+# keys are the first values in the colon-separated pairs and values are second
+#       i.e. "five": 5 -> "five" is key and 5 is value
+
+list_example.append(999)
+print(list_example) # prints "[3, 3, 3.3, '3', 999]"
+# lists are mutable so its append() function adds a value
+# (the python interpreter may use '' instead of "" for strings but it doesn't matter)
+
+print(list_example * 2) # prints [3, 3, 3.3, '3', 999, 3, 3, 3.3, '3', 999]
+# like strings, you can multiply all collections by an integer to repeat it
+
+print(list_example + ["additional", "stuff"]) # prints "[3, 3, 3.3, '3', 999, 'additional', 'stuff']"
+# also like strings, lists can be concatenated (only with the same type though)
+
+print(list_example.pop()) # prints "999"
+# pop() removes last value and returns it
+print(list_example) # prints "[3, 3, 3.3, '3']"
+# Note that the 999 has been removed from the list
+
+list_example.remove(3.3)
+print(list_example) # prints "[3, 3, '3']"
+# you can remove elements by popping by index or removing by element
+#       If an element is contained twice, it will remove first instance
+
+## More functions for lists and dictionaries can be found in the API
+## You will need to use the API
